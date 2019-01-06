f648d661e65c4bd8c6ce8d52d1ce022c44b2467d
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-01-07 00:40:01 +0100

    emscripten: set ALLEGRO_NO_PRESERVE_TEXTURE
    Doesn't seem to break anything there despite of using SDL2 backend.

--- src/libsuperderpy.c
+++ src/libsuperderpy.c
@@ -223,12 +223,11 @@ SYMBOL_EXPORT struct Game* libsuperderpy_init(int argc, char** argv, const char*
 	al_android_set_apk_fs_interface();
 #endif
 
-#if !defined(ALLEGRO_ANDROID) && !defined(ALLEGRO_IPHONE) && !defined(ALLEGRO_SDL)
+#if !defined(ALLEGRO_ANDROID) && !defined(ALLEGRO_IPHONE) && (!defined(ALLEGRO_SDL) || defined(__EMSCRIPTEN__))
 	// We're always using OpenGL which already preserves textures on its own, so avoid
 	// excessive RAM usage by not backuping the bitmaps when not necessary.
 	// Android and iOS can threw out the context, so bitmaps need to be preserved there.
-	// SDL2 backend seems to have some workaround for old SDL bug, worth looking into.
-	// This will have to be revisited on mobile GNU/Linux platforms.
+	// Something should be eventually done about SDL2 backend as well.
 	al_add_new_bitmap_flag(ALLEGRO_NO_PRESERVE_TEXTURE);
 #endif
 
