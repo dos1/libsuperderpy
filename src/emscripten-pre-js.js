d75327976448946b4a4eb47bdecfced83dba35d4
Sebastian Krzyszkowiak <dos@dosowisko.net>
2020-03-02 03:23:01 +0100

    emscripten-pre-js: remove autoplay workaround
    It didn't work with recent emscripten versions anyway. A better fix
    is coming to emscripten's SDL port.

--- src/emscripten-pre-js.js
+++ src/emscripten-pre-js.js
@@ -3,12 +3,3 @@ if (!Module) Module = (typeof Module !== 'undefined' ? Module : null) || {};
 // Disable image and audio decoding
 Module.noImageDecoding = true;
 Module.noAudioDecoding = true;
-
-// autoplay workaround
-document.addEventListener("click", function() {
-  try {
-    if (!SDL2 || !SDL2.audioContext || !SDL2.audioContext.resume) return;
-      SDL2.audioContext.resume();
-    } catch (err) {}
-  }
-);
