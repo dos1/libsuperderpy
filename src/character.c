a71d0bb0bb1d74762420e4a6df0423f67bcde3eb
Sebastian Krzyszkowiak <dos@dosowisko.net>
2022-07-10 00:49:41 +0200

    character: Fix frame count handling in SwitchSpritesheet

--- src/character.c
+++ src/character.c
@@ -85,7 +85,7 @@ SYMBOL_EXPORT void SelectSpritesheet(struct Game* game, struct Character* charac
 SYMBOL_EXPORT void SwitchSpritesheet(struct Game* game, struct Character* character, char* name) {
 	int pos = character->pos;
 	SelectSpritesheet(game, character, name);
-	if (pos < character->spritesheets->frame_count && !character->spritesheet->stream) {
+	if (pos < character->spritesheet->frame_count && !character->spritesheet->stream) {
 		character->pos = pos;
 		character->frame = &character->spritesheet->frames[character->pos];
 	}
